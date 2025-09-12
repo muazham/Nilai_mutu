9ddeeb67dea96f0177c484b34ad60cd0727c4600
muazham <muazhamazam@gmail.com>
2025-09-12 10:44:55 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <header>
+        <h1>kingz4m</h1>
+        <navigasi>
+            <a href="indx.php">Beranda</a>
+            <a href="indx.php">Tentang</a>
+            <a href="indx.php">Konak</a>
+        </nav>
+</header>    
+</body>
+</html>
