b5712d49c9e91dd09f6be83921158d9560bfc12d
muazham <muazhamazam@gmail.com>
2025-09-12 10:47:11 +0700

    Add files via upload

---
+++ Tentang.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+?>
+<h2>Tentang Website</h2>
+<p>Website ini dibuat untuk orang gabut, termasuk bingung, gakjelas, entah maungapain dan require.</p>
+<?php
+require 'footer.php';
+?>
