540daa10a23f732484c4092721e401b20c2116ef
muazham <muazhamazam@gmail.com>
2025-09-12 10:45:39 +0700

    Add files via upload

---
+++ Kontak.php
@@ -0,0 +1,16 @@
+<?php
+require 'header.php';
+?>
+<h2>Hubungi kami</h2>
+<formulir>
+    <label>Nama:</label><br>
+    <input type ="text" name="nama"><br><br>
+
+    <label>pesen:</label><br>
+    <texterea name="pesan"></texarea><br></br>
+
+    <button type="submit">kirim</button>
+</form>
+<?php
+require 'footer.php';
+?>
