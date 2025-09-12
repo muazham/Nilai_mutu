bd69f5ad02cc608cb6666a458c30db73013e33fd
muazham <muazhamazam@gmail.com>
2025-09-12 10:45:15 +0700

    Add files via upload

---
+++ indx.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+?>
+<h2>Selamat Datang di Website cara apaaja</h2>
+<p>Ini adalah halaman utama (Home). Website ini mengajar kan kalian untuk menjadi the king.</p>
+<?php
+require 'footer.php';
+?>
